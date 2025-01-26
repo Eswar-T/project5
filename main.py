ac55c2481163a1f67486f7f228a4f1e113dc07c0
Eswar-T <t.eswarvarun@gmail.com>
2025-01-26 14:36:29 +0530

    added titles

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as lit
-
+import pandas
 
 lit.set_page_config(layout="wide")
 col1, col2 = lit.columns(2)
@@ -14,4 +14,19 @@ with col2:
 
 content1 = "he standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham."
 lit.write(content1)
+
+col3, col4 = lit.columns(2)
+
+df = pandas.read_csv("data.csv",sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        lit.header(row["title"])
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        lit.header(row["title"])
+
+
 
